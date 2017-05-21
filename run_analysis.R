9eed509c4b153e18a78c3cf753ab49cf9cd181d7
Diego Freitas <diego.lins.freitas@gmail.com>
2017-05-21 09:24:24 -0400

    fix step 2 and 3 of the assignment

--- run_analysis.R
+++ run_analysis.R
@@ -13,15 +13,6 @@ features_names <- features[,2]
 
 activities_labels <- read.table("UCI HAR Dataset/activity_labels.txt", header = F)
 
-# 4. Appropriately labels the data set with descriptive variable names.
-colnames(x_train) <- features_names
-colnames(x_test) <- features_names
-
-colnames(y_train) <- c("ACTIVITY")
-colnames(y_test) <- c("ACTIVITY")
-
-colnames(sub_train) <- c("SUBJECT")
-colnames(sub_test) <- c("SUBJECT")
 
 # 1. Merges the training and the test sets to create one data set.
 
@@ -30,15 +21,32 @@ merged_test <- cbind(y_test, sub_test, x_test )
 
 tidy_data <- rbind(merged_train, merged_test)
 
-# 3. Uses descriptive activity names to name the activities in the data set
-tidy_data$ACTIVITY <- factor(tidy_data$ACTIVITY, activities_labels[,1], activities_labels[,2])
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-measurement_mean <- colwise(mean)(tidy_data[,3:563])
-measurement_sd <- colwise(sd)(tidy_data[,3:563])
+selected_features <- sapply(features_names, function(name){
+  grepl("(mean|std)\\(\\)", name)
+})
+
+#keep Activity and Subject
+features_names <- features_names[selected_features]
+selected_features <- append(selected_features, c(TRUE, TRUE), 0)
+tidy_data <- tidy_data[,selected_features]
+
+# 3. Uses descriptive activity names to name the activities in the data set
+tidy_data[,1] <- factor(tidy_data[,1], activities_labels[,1], activities_labels[,2])
+
+
+# 4. Appropriately labels the data set with descriptive variable names.
+features_names <- sapply(features_names,function(s) {
+  readable<- gsub("\\(\\)", "", s)
+  toupper(gsub("-", "_", readable))
+})
+colnames(tidy_data) <- append(features_names, c("ACTIVITY", "SUBJECT"), 0)
+
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-summary_df <- ddply(tidy_data, .(ACTIVITY, SUBJECT), colwise(mean))
+mean_values <- ddply(tidy_data, .(ACTIVITY, SUBJECT), colwise(mean))
 
-write.table(summary_df,"averages.txt",  row.name=FALSE)
+write.table(mean_values,"averages.txt",  row.name=FALSE)
 
+str(mean_values)
